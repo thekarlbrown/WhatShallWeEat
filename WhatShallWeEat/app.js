301c7fd11bfc6db59060ab7f582745df75a0fe06
Karl Brown <karlbrownthinks@gmail.com>
2020-03-21 17:16:36 +0000

    Modified EJS routing to accomodate Angular routing

--- WhatShallWeEat/app.js
+++ WhatShallWeEat/app.js
@@ -23,30 +23,12 @@ app.use(cookieParser());
 // Serve static files associated with index.html
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Serve index.html built by Angular
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-// Serve API's 
+//Serve API's
 app.use('/api/restaurants', restaurants);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Serve index.html built by Angular
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 module.exports = app;
