974b579a0fde3cc41c2ab99b8362ab72f7509146
Karl Brown <karlbrownthinks@gmail.com>
2020-03-21 18:25:03 -0400

    Added attempt to obtain geolocation data

--- WhatShallWeEat-UI/src/pages/list/list.component.ts
+++ WhatShallWeEat-UI/src/pages/list/list.component.ts
@@ -8,11 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListComponent implements OnInit {
   uuid: string;
+  location: {};
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.uuid = this.route.snapshot.params.uuid;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.obtainLocationInfo);
+    } else {
+      this.location = { latitude: '38.957599', longitude: '-77.175630' };
+    }
+  }
+
+  obtainLocationInfo(position) {
+    this.location = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    };
+
+    console.log(this.location);
   }
 
 }
