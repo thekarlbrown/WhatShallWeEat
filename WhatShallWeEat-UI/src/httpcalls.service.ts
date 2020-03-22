1fd3e7031669aa7b501c4f2a4893ae66af59751e
Karl Brown <karlbrownthinks@gmail.com>
2020-03-22 12:07:46 -0400

    Attempt to correctly pipe results

--- WhatShallWeEat-UI/src/httpcalls.service.ts
+++ WhatShallWeEat-UI/src/httpcalls.service.ts
@@ -10,6 +10,10 @@ export class HttpCallsService {
   constructor(private http: HttpClient) { }
 
   getRestaurantsFromPlacesAPI(lat: number, lng: number) {
-    return this.http.get(`https://100.27.23.136:8443/api/restaurants/list/location=${lat},${lng}`);
+    return this.http.get(`https://100.27.23.136:8443/api/restaurants/list/location=${lat},${lng}`)
+    .pipe(
+      map((result: any) => result.results),
+      map(results => results.name)
+    );
   }
 }
